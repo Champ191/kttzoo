4237646808d5664745b718f976de734de67123f7
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-06-30 20:30:48 -0300

    feat: update main.ts to use environment configuration for application port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { environment } from './configs/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(environment.APP_PORT);
 }
-bootstrap();
+
+void bootstrap();
