1330fdb1d672e63a4e955dc48a1e61eb4430be94
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-06-30 20:31:35 -0300

    feat: update AppController to handle translation requests with AI and code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TranslateDto } from './translate.dto';
 
-@Controller()
+@Controller('translate')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('ai')
+  async translateWithAi(@Body() body: TranslateDto) {
+    return this.appService.translateWithAi(body);
+  }
+
+  @Post('code')
+  async translateWithCode(@Body() body: TranslateDto) {
+    return this.appService.translateWithCode(body);
   }
 }
