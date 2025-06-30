28b1ed94d07b018bbf0a7acc09f88a15e3a033b1
João Victor Grille Santiago <grillejv+contas@gmail.com>
2025-06-30 20:32:09 -0300

    feat: integrate GeminiClientModule and add TranslateDto class

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GeminiClientModule } from './infra/gemini-client/gemini-client.module';
 
 @Module({
-  imports: [],
+  imports: [GeminiClientModule],
   controllers: [AppController],
   providers: [AppService],
 })
